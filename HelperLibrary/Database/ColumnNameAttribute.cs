d73277e494673a60e420fca79302c792131fde56
Nabrezzelt <nabrezzelt@web.de>
2017-09-25 10:47:14 +0200

    Created Tablemapper-Class

---
+++ HelperLibrary/Database/ColumnNameAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelperLibrary.Database
+{
+    [AttributeUsage(AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+    sealed class ColumnNameAttribute : Attribute
+    {
+        // See the attribute guidelines at 
+        //  http://go.microsoft.com/fwlink/?LinkId=85236
+
+        public ColumnNameAttribute(string positionalString)
+        {
+            ColumnName = positionalString;            
+        }
+
+        public string ColumnName { get; }
+    }
+}
---
+++ HelperLibrary/Database/TableMapper.cs
@@ -0,0 +1,43 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+
+namespace HelperLibrary.Database
+{
+    class TableMapper<T> where T : new()
+    {
+        public static List<T> MapReaderToTable(MySqlDataReader reader)
+        {
+            List<T> tableItems = new List<T>();
+
+            while(reader.Read())
+            {
+                T tableRow = new T();
+
+                foreach (var prop in typeof(T).GetProperties())
+                {
+                    object[] attributes = prop.GetCustomAttributes(true);
+
+                    string columnName = prop.Name;
+
+                    foreach (object attribute in attributes)
+                    {
+                        ColumnNameAttribute columnNameAttr = attribute as ColumnNameAttribute;
+
+                        if(columnNameAttr != null && String.IsNullOrEmpty(columnNameAttr.ColumnName))
+                        {
+                            columnName = columnNameAttr.ColumnName;
+                        }
+                    }                    
+
+                    tableRow.GetType().GetProperty(prop.Name).SetValue(prop, Convert.ChangeType(reader[columnName], prop.PropertyType), null);                                        
+                }
+                
+                tableItems.Add(tableRow);
+            }
+
+            reader.Close();
+            return tableItems;
+        }
+    }
+}
