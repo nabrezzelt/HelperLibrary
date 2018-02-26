318b3f669ccfcfb13295de187bbee04ff6fb250c
Nabrezzelt <nabrezzelt@web.de>
2018-02-26 11:22:59 +0100

    Added Random-String-Generator form charset method

--- HelperLibrary/Cryptography/HashManager.cs
+++ HelperLibrary/Cryptography/HashManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -6,6 +7,7 @@ namespace HelperLibrary.Cryptography
 {
     public class HashManager
     {
+        private static readonly Random Random = new Random();
         public static string HashSha512(string text)
         {
             if (string.IsNullOrEmpty(text))
@@ -20,7 +22,7 @@ namespace HelperLibrary.Cryptography
         public static string HashSha512(byte[] data)
         {
             using (var sha = new SHA512Managed())
-            {                
+            {
                 byte[] hash = sha.ComputeHash(data);
                 return BitConverter.ToString(hash).Replace("-", String.Empty);
             }
@@ -40,7 +42,7 @@ namespace HelperLibrary.Cryptography
         public static string HashSha384(byte[] data)
         {
             using (var sha = new SHA384Managed())
-            {               
+            {
                 byte[] hash = sha.ComputeHash(data);
                 return BitConverter.ToString(hash).Replace("-", String.Empty);
             }
@@ -60,7 +62,7 @@ namespace HelperLibrary.Cryptography
         public static string HashSha256(byte[] data)
         {
             using (var sha = new SHA256Managed())
-            {                
+            {
                 byte[] hash = sha.ComputeHash(data);
                 return BitConverter.ToString(hash).Replace("-", String.Empty);
             }
@@ -80,7 +82,7 @@ namespace HelperLibrary.Cryptography
         public static string HashSha1(byte[] data)
         {
             using (var sha = new SHA1Managed())
-            {                
+            {
                 byte[] hash = sha.ComputeHash(data);
                 return BitConverter.ToString(hash).Replace("-", String.Empty);
             }
@@ -100,7 +102,7 @@ namespace HelperLibrary.Cryptography
         public static string HashMd5(byte[] data)
         {
             using (MD5 md5 = MD5.Create())
-            {                
+            {
                 byte[] hash = md5.ComputeHash(data);
                 return BitConverter.ToString(hash).Replace("-", String.Empty);
             }
@@ -131,12 +133,19 @@ namespace HelperLibrary.Cryptography
                 rng.GetBytes(tokenData);
 
                 randomToken.Append(Convert.ToBase64String(tokenData));
-                
+
                 return randomToken.ToString()
                     .Replace("=", String.Empty)
                     .Replace("+", String.Empty)
-                    .Replace("/", String.Empty);                
-            }                       
+                    .Replace("/", String.Empty);
+            }
         }
+
+        public static string GenerateRandomStringFromChars(int length, string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789")
+        {
+            return new string(Enumerable.Repeat(chars, length)
+                               .Select(s => s[Random.Next(s.Length)]).ToArray());
+        }
+
     }
 }
